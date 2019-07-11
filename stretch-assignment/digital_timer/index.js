9797c8e7b9afed247e4e8ddc104f8195f3960a14
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-07-10 21:31:47 -0500

    reached 10:00 and styled to red

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,6 +4,10 @@ const ones = digits.querySelector('#secondOnes');
 const tenths = digits.querySelector('#msTens');
 const hundredths = digits.querySelector('#msHundreds');
 
+function flashRed() {
+    digits.classList.toggle('redDigit');
+}
+
 function increment() {
     // INCREASE HUNDREDTHS
     if (hundredths.textContent === '-') hundredths.textContent = '1';
@@ -19,6 +23,13 @@ function increment() {
     if (hundredths.textContent === '0' && tenths.textContent === '0' && ones.textContent === '-') ones.textContent = '1';
     else if (hundredths.textContent === '0' && tenths.textContent === '0' && ones.textContent === '9') ones.textContent = '0';
     else if (hundredths.textContent === '0' && tenths.textContent === '0') ones.textContent++;
+    
+    if (hundredths.textContent === '0' && tenths.textContent === '0' && ones.textContent === '0') {
+        tens.textContent = '1';
+        clearInterval(timer);
+        flashRed();
+        setInterval(flashRed, 300);
+    }
 }
 
 let timer = window.setInterval(increment, 10);
